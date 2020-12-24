fa5c4e18325f03205e369af2d3947d3e8f8ced27
Daniel Medina <daniel@medina.com>
2020-12-24 12:30:02 -0600

    Added funcionality to select port

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const multipart = require('connect-multiparty');
 const crypto = require('crypto');
 const cors = require('cors');
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 let multipartMiddleware = multipart({uploadDir: './img'});
@@ -66,6 +68,6 @@ app.get('**', (req, res) => {
 	res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.listen(80, () => {
-	console.log(`Listening on http://localhost`)
+app.listen(port, () => {
+	console.log(`Listening on http://localhost:${port}`)
 }); 
