81e8833a4d4df9010952381bd1ae88f3c8dc2b96
Daniel Medina <daniel@medina.com>
2020-12-24 12:34:00 -0600

    fixed bugs

--- index.js
+++ index.js
@@ -68,6 +68,6 @@ app.get('**', (req, res) => {
 	res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.listen(port, () => {
+app.listen(port, '127.0.0.1',  () => {
 	console.log(`Listening on http://localhost:${port}`)
 }); 
